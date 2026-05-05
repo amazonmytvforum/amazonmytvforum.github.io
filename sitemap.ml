179aa2adefb8bd1eaa72b4d3b00363b71af4f000
amazonmytvforum <gethelpassistant@gmail.com>
2026-05-06 00:27:41 +0530

    Update sitemap.ml

--- sitemap.ml
+++ sitemap.ml
@@ -1,1 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
+  <!-- Homepage / Main Page -->
+  <url>
+    <loc>https://amazonmytvforum.github.io/</loc>
+    <lastmod>2026-05-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <!-- Enter Code Page -->
+  <url>
+    <loc>https://amazonmytvforum.github.io/amazon-mytv-code</loc>
+    <lastmod>2026-05-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+
+  <!-- Not Working / Troubleshooting Page -->
+  <url>
+    <loc>https://amazonmytvforum.github.io/amazon-mytv-not-working</loc>
+    <lastmod>2026-05-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+
+  <!-- Activate TV Page -->
+  <url>
+    <loc>https://amazonmytvforum.github.io/amazon-mytv-activate</loc>
+    <lastmod>2026-05-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <!-- Supported Devices Page -->
+  <url>
+    <loc>https://amazonmytvforum.github.io/amazon-mytv-devices</loc>
+    <lastmod>2026-05-06</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+
+</urlset>
